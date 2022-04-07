8b17f0c73a4f406bc020718344a4b587de05c5b6
UlisesGallardo <0229261@up.edu.mx>
2022-04-07 11:26:49 -0500

    nuevas practicas

---
+++ Practica15/Visual C-Sharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sPuerto = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDato = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sPuerto
+            // 
+            this.sPuerto.BaudRate = 4800;
+            this.sPuerto.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPuerto_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "El último dato recibido por el puerto serial (en decimal) fue:";
+            // 
+            // lblDato
+            // 
+            this.lblDato.AutoSize = true;
+            this.lblDato.Location = new System.Drawing.Point(340, 86);
+            this.lblDato.Name = "lblDato";
+            this.lblDato.Size = new System.Drawing.Size(35, 13);
+            this.lblDato.TabIndex = 1;
+            this.lblDato.Text = "label2";
+            this.lblDato.Click += new System.EventHandler(this.lblDato_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 173);
+            this.Controls.Add(this.lblDato);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Examen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort sPuerto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDato;
+    }
+}
+
---
+++ Practica15/Visual C-Sharp/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO.Ports;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort spPuertoSerie = new SerialPort(
+           "COM2", 4800, Parity.None, 8, StopBits.Two);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false; 
+            spPuertoSerie.DataReceived += new SerialDataReceivedEventHandler(sPuerto_DataReceived);
+
+            try //intenta hacer lo que hay dentro de este try
+            {
+                //sPuerto.Open(); //abrir el puerto serial
+                spPuertoSerie.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR AL ABRIR EL PUERTO SERIAL\n\n" + ex); //Me muestra el error
+                Application.Exit(); //cierra el programa
+            }
+            if (spPuertoSerie.IsOpen)
+            {
+                //MessageBox.Show("TEXTO","TÍTULO");
+                //A continuación se muestra la información del puerto (si se desea cambiar se debe hacer desde las propiedades del puerto)
+                MessageBox.Show("El puerto " + spPuertoSerie.PortName + " se abrió con una velocidad de " + spPuertoSerie.BaudRate + "\nParidad: " + spPuertoSerie.Parity + "\nNúmero de bits: " + spPuertoSerie.DataBits + "\nBits de parada: " + spPuertoSerie.StopBits, "Información de la conexión");
+            }
+        }
+
+        //public static void DataReceivedHandler
+        private void sPuerto_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            int Dato = spPuertoSerie.ReadByte();
+            double voltaje = Dato * 5 / 255.0;
+            Console.WriteLine(voltaje);
+            this.lblDato.Text = Convert.ToString(Dato);
+            //spPuertoSerie.Write("C"+Convert.ToString(voltaje));
+
+            string v = Convert.ToString(voltaje);
+            int n = v.Length + 1;
+
+            Byte[] info = new Byte[n];
+            info[0] = Convert.ToByte('C');
+            int j = 1;
+            foreach (char c in v)
+            {
+                info[j] = Convert.ToByte(c);
+                j++;
+            }
+
+            spPuertoSerie.Write(info,0,n);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblDato_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
